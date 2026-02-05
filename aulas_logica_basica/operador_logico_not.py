5ef54846b91c9630f8359d0acfbed64f921f455f
Danilo Diniz <dgdevbr@gmail.com>
2026-02-05 19:08:27 -0300

    operador lógico not exemplos

---
+++ aulas_logica_basica/operador_logico_not.py
@@ -0,0 +1,10 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+# senha = input('Digite sua senha: ')
+#
+# if senha != '123456':
+#     print('Senha invalida!')
+print(not True) # False
+print(not False) # True
