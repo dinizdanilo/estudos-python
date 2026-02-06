8469119429e06f69e31a3aab4e3d8ac3b7132249
Danilo Diniz <dgdevbr@gmail.com>
2026-02-06 20:43:36 -0300

    duas resoluções para exercicio de saudação

--- exercicios_logica_basica/saudacao_apropriada.py
+++ exercicios_logica_basica/saudacao_apropriada.py
@@ -1,12 +1,25 @@
 hora = input('Que horas são agora?: ')
 
+# try:
+#     hora = int(hora)
+#     if hora in range(0, 12):
+#         print('Bom dia!')
+#     elif hora in range(12, 18):
+#         print('Boa tarde!')
+#     elif hora in range(18, 24):
+#         print('Boa noite!')    
+# except ValueError: 
+#     print('Digite um horário válido!')
+    
 try:
     hora = int(hora)
-    if hora in range(0, 12):
+    if hora >= 0 and hora <= 11:
         print('Bom dia!')
-    elif hora in range(12, 18):
+    elif hora >= 12 and hora <= 17:
         print('Boa tarde!')
+    elif hora >= 18 and hora <= 23:
+        print('Boa noite!')
     else:
-        print('Boa noite!')    
-except: 
-    print('Digite um horário válido!')
+        print('Digite um horário válido!')        
+except ValueError: 
+    print('Digite um horário válido!')    
