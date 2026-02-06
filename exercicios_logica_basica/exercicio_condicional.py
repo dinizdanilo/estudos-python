c4aaf85607afbaebe77d32f5b54160d475bf375f
Danilo Diniz <dgdevbr@gmail.com>
2026-02-06 10:36:16 -0300

    nova resolução

--- exercicios_logica_basica/exercicio_condicional.py
+++ exercicios_logica_basica/exercicio_condicional.py
@@ -1,14 +1,29 @@
 nome = input('Digite seu nome: ')
 idade = input('Digite sua idade: ')
 
-if nome == '' or idade == '':
-    print('Desculpe, você deixou campos vazios.')
-else:
-    print(f'Seu nome é {nome}')    
+# if nome == '' or idade == '':
+#     print('Desculpe, você deixou campos vazios.')
+# else:
+#     print(f'Seu nome é {nome}')    
+#     print(f'Seu nome ao contrário é {nome[::-1]}')
+#     print(f'Seu nome tem {len(nome)} letras')
+#     if ' ' in nome:
+#         print('Seu nome contém espaços.')
+#     else:
+#         print('Seu nome não contém espaços.')
+#     print(f'A primeira letra do seu nome é {nome[0]}')
+#     print(f'A ultima letra do seu nome é {nome[-1]}') 
+
+if nome and idade:
+    print(f'Seu nome é {nome}')
     print(f'Seu nome ao contrário é {nome[::-1]}')
+    print(f'Seu nome tem {len(nome)} letras') 
+    print(f'A primeira letra do seu nome é {nome[0]}')
+    print(f'A ultima letra do seu nome é {nome[-1]}')
     if ' ' in nome:
         print('Seu nome contém espaços.')
     else:
         print('Seu nome não contém espaços.')
-    print(f'A primeira letra do seu nome é {nome[0]}')
-    print(f'A ultima letra do seu nome é {nome[-1]}') 
+else:
+    print('Desculpe, você deixou campos vazios.')
+         
