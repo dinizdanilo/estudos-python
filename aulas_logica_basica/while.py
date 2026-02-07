2a503b3e3d98b79a3596cf8e37765524a7632557
Danilo Diniz <dgdevbr@gmail.com>
2026-02-07 13:15:53 -0300

    while

---
+++ aulas_logica_basica/while.py
@@ -0,0 +1,65 @@
+"""
+Repetições 
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+
+# condicao = True
+
+# while condicao:
+#     nome = input('Qual o seu nome? ')
+#     print(f'Seu nome é {nome}')
+    
+#     if nome == 'Sair':
+#         break
+
+# print('Acabou')    
+
+# contador = 0
+
+# while contador < 10:
+#     contador = (contador + 1)
+#     print(contador)
+
+# print('Acabou')   
+ 
+# contador = 10
+
+# contador *= 2
+# print(contador)
+
+# contador = 0
+
+# while contador <= 100:
+#     contador += 1
+
+#     if contador == 6:
+#         print('Não vou mostrar o 6')
+#         continue
+    
+#     if contador <= 10 and contador <= 27:
+#         print('Não vou mostar o ', contador)
+#         continue
+    
+#     print(contador)
+    
+#     if contador == 40:
+#         break
+    
+# print('Acabou')   
+
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+while linha <= qtd_linhas:
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=} {coluna=}')
+        coluna += 1
+    linha += 1
+    
+print('Acabou')    
