d1600536854c17bb8561f015df909c6b8a8f3c6b
Danilo Diniz <dgdevbr@gmail.com>
2026-02-06 22:41:34 -0300

    exercicio par ou impar

--- exercicios_logica_basica/par_ou_impar.py
+++ exercicios_logica_basica/par_ou_impar.py
@@ -3,8 +3,8 @@ numero = input('Digite um número inteiro: ')
 try:
     numero = int(numero)
     if numero % 2 == 0:
-        print('O número que você digitou é par.')
+        print(f'O número {numero} que você digitou é par.')
     else:
-        print('O número que você digitou é ímpar.')
+        print(f'O número {numero} que você digitou é ímpar.')
 except ValueError:
     print('O que você digitou não é um número inteiro.')
