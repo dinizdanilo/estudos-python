71552abf1540e0e700dffde203b5cd060fc9edac
Danilo Diniz <dgdevbr@gmail.com>
2026-02-07 16:43:59 -0300

    contador de espaços em frases

---
+++ exercicios_logica_basica/contador_espaco.py
@@ -0,0 +1,15 @@
+frase = input('Digite uma frase curta: ')
+
+contador = 0
+indice = 0
+
+while indice < len(frase):
+    letra = frase[indice]
+    indice += 1
+    if letra == ' ':
+        contador += 1
+
+if contador > 0:
+    print(f'A quantida de de espaços na sua frase é {contador}')    
+else:
+    print(f'Sua frase não contém espaços') 
