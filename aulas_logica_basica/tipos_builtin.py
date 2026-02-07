ed6fed4be40d307a5515c69780ca5edade023ee3
Danilo Diniz <dgdevbr@gmail.com>
2026-02-07 12:08:24 -0300

    tipos imutáveis

---
+++ aulas_logica_basica/tipos_builtin.py
@@ -0,0 +1,8 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'Danilo Diniz'
+# outra_variavel = f'{string[:3]} ABC{string[4:]}'
+# string[3] = 'ABC'
+print(string.zfill(10))
