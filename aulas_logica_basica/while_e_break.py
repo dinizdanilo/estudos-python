2c5954997cb07ca883ed22a6a94092c3f9877522
Danilo Diniz <dgdevbr@gmail.com>
2026-02-07 12:50:20 -0300

    operadores aritmeticos + sinal de atribuição no while

--- aulas_logica_basica/while_e_break.py
+++ aulas_logica_basica/while_e_break.py
@@ -3,6 +3,8 @@ Repetições
 while (enquanto)
 Executa uma ação enquanto uma condição for verdadeira
 Loop infinito -> Quando um código não tem fim
+Operadores de atribuição
+= += -= *= /= //= **= %=
 """
 
 # condicao = True
@@ -16,10 +18,15 @@ Loop infinito -> Quando um código não tem fim
 
 # print('Acabou')    
 
-contador = 0
+# contador = 0
 
-while contador < 10:
-    contador = (contador + 1)
-    print(contador)
+# while contador < 10:
+#     contador = (contador + 1)
+#     print(contador)
 
-print('Acabou')    
+# print('Acabou')   
+ 
+contador = 10
+
+contador *= 2
+print(contador)
