29291ccb3a75d90becc6bb60e05cb47563990cb6
Danilo Diniz <dgdevbr@gmail.com>
2026-02-07 13:03:14 -0300

    condições dentro de um while

--- aulas_logica_basica/while_e_break.py
+++ aulas_logica_basica/while_e_break.py
@@ -26,7 +26,28 @@ Operadores de atribuição
 
 # print('Acabou')   
  
-contador = 10
+# contador = 10
 
-contador *= 2
-print(contador)
+# contador *= 2
+# print(contador)
+
+contador = 0
+
+while contador <= 100:
+    contador += 1
+
+    if contador == 6:
+        print('Não vou mostrar o 6')
+        continue
+    
+    if contador <= 10 and contador <= 27:
+        print('Não vou mostar o ', contador)
+        continue
+    
+    print(contador)
+    
+    if contador == 40:
+        break
+    
+print('Acabou')     
+
