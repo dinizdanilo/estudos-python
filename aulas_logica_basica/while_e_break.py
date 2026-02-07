daadfba922c9e0ddbbfbe833307537d202ffa590
Danilo Diniz <dgdevbr@gmail.com>
2026-02-07 13:15:04 -0300

    while dentro de um while

--- aulas_logica_basica/while_e_break.py
+++ aulas_logica_basica/while_e_break.py
@@ -31,23 +31,35 @@ Operadores de atribuição
 # contador *= 2
 # print(contador)
 
-contador = 0
+# contador = 0
 
-while contador <= 100:
-    contador += 1
+# while contador <= 100:
+#     contador += 1
 
-    if contador == 6:
-        print('Não vou mostrar o 6')
-        continue
+#     if contador == 6:
+#         print('Não vou mostrar o 6')
+#         continue
     
-    if contador <= 10 and contador <= 27:
-        print('Não vou mostar o ', contador)
-        continue
+#     if contador <= 10 and contador <= 27:
+#         print('Não vou mostar o ', contador)
+#         continue
     
-    print(contador)
+#     print(contador)
     
-    if contador == 40:
-        break
+#     if contador == 40:
+#         break
     
-print('Acabou')     
+# print('Acabou')   
 
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+while linha <= qtd_linhas:
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=} {coluna=}')
+        coluna += 1
+    linha += 1
+    
+print('Acabou')    
