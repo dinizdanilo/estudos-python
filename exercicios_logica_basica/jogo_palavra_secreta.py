8fa590d6b4e581c448d803af961c7abddee76f29
Danilo Diniz <dgdevbr@gmail.com>
2026-02-08 14:03:19 -0300

    jogo palavra secreta

---
+++ exercicios_logica_basica/jogo_palavra_secreta.py
@@ -0,0 +1,30 @@
+palavra_secreta = ('automovel')
+
+letras_acertadas = ''
+tentativas = 0
+palavra_formatada = ''
+
+while palavra_formatada != palavra_secreta:
+    palavra_formatada = ''
+    letra_digitada = input('Digite uma letra: ').lower()
+    
+    if len(letra_digitada) > 1:
+        print('Digite apenas uma letra.')
+        continue
+    
+    tentativas += 1
+    
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letra_digitada
+            
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_formatada += letra_secreta
+        else:
+            palavra_formatada += '*'   
+           
+    print(palavra_formatada)
+            
+print('VOCÊ GANHOU PARABENS!!')        
+print(f'A palavra secreta era: "{palavra_secreta}"')
+print(f'A quantidade de tentativas foi: {tentativas}')       
